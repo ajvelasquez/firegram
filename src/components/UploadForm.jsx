466018e5a9cab5dcf432ddb59002de591b7b1a25
avelasquez <amalio1212@gmail.com>
2021-01-23 23:05:14 -0400

    create upload form component

---
+++ src/components/UploadForm.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const UploadForm = () => {
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
+
+  const changeHandler = (e) => {
+    let selected = e.target.files[0];
+
+    const types = ["image/png", "image/jpeg"];
+
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+      setError(null);
+      return;
+    }
+
+    setFile(null);
+    setError("Plese select an image file (PNG or JPEG)");
+  };
+
+  return (
+    <form>
+      <input type="file" onChange={changeHandler} />
+      <div className="output">
+        {error && <div className="error">{error}</div>}
+      </div>
+    </form>
+  );
+};
+
+export default UploadForm;
