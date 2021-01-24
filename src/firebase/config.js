18f8badfd6efb2a00de2616a04bbe8c3e088d6c8
avelasquez <amalio1212@gmail.com>
2021-01-23 22:50:17 -0400

    set up the project

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDcOZlOL281VeWQb2T6YFy04yjAH7LYUK0",
+  authDomain: "firegram-e26d8.firebaseapp.com",
+  projectId: "firegram-e26d8",
+  storageBucket: "firegram-e26d8.appspot.com",
+  messagingSenderId: "872284245661",
+  appId: "1:872284245661:web:345faebfb33a6fc98b0e74",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
