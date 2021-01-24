a1dc77585284fb59d6fa0f86d88ad4e2a36d4846
avelasquez <amalio1212@gmail.com>
2021-01-24 01:51:33 -0400

    create modal and add animations

--- src/components/ImageGrid.jsx
+++ src/components/ImageGrid.jsx
@@ -1,17 +1,31 @@
+import { motion } from "framer-motion";
 import React from "react";
 import useFirestore from "../hooks/useFirestore";
 
-const ImageGrid = () => {
-  const { docs } = useFirestore('images');
-  return <div className="img-grid">
-      {docs && (
-          docs.map((doc) => (
-              <div className="img-wrap" key={doc.id}>
-                  <img src={doc.url} alt="Uploaded pic"/>
-              </div>
-          ))
-      )}
-  </div>;
+const ImageGrid = ({ setSelectedImg }) => {
+  const { docs } = useFirestore("images");
+  return (
+    <div className="img-grid">
+      {docs &&
+        docs.map((doc) => (
+          <motion.div
+            layout
+            whileHover={{ opacity: 1 }}
+            className="img-wrap"
+            key={doc.id}
+            onClick={() => setSelectedImg(doc.url)}
+          >
+            <motion.img
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1 }}
+              src={doc.url}
+              alt="Uploaded pic"
+            />
+          </motion.div>
+        ))}
+    </div>
+  );
 };
 
 export default ImageGrid;
---
+++ src/components/Modal.jsx
@@ -0,0 +1,27 @@
+import { motion } from "framer-motion";
+import React from "react";
+
+const Modal = ({ selectedImg, setSelectedImg }) => {
+  const handleClick = (e) => {
+    if (e.target.classList.contains("backdrop")) {
+      setSelectedImg(null);
+    }
+  };
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="backdrop"
+      onClick={handleClick}
+    >
+      <motion.img
+        initial={{ y: "-100vh" }}
+        animate={{ y: 0 }}
+        src={selectedImg}
+        alt="Enlarged pic"
+      />
+    </motion.div>
+  );
+};
+
+export default Modal;
